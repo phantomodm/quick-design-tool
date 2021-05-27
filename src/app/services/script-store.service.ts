daf5124572ed22fe78002331bdcfa78c146596da
PhantomOdm <31314894+phantomodm@users.noreply.github.com>
2021-05-27 17:24:15 -0400

    Add script to runtime

--- src/app/services/script-store.service.ts
+++ src/app/services/script-store.service.ts
@@ -12,7 +12,7 @@ interface Scripts {
 export const ScriptStore:Scripts[] = [
   {name: 'jQuery', src:'https://code.jquery.com/jquery-3.6.0.min.js', integrity:"sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=", crossorigin: "anonymous"},
   {name:'bootstrap',src:"https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js",integrity:"sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0",crossorigin:"anonymous"},
-
+  {name: 'boot-icon', src: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" }
 ]
 
 @Injectable({
